0c9c6f070dcdafad7f971753cf01ac82080fab47
Pranav Joseph <pranavjoseph2005@gmail.com>
2025-05-22 16:33:09 -0500

    set up supabase connection and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useState }from 'react'
 import Link from 'next/link'
 
 const Home = () => {
+
+  const [loggedIn, setLoggedIn] = useState(false);
   return <div>
     <Link
       href="/dashboard">
       Dashboard
     </Link>
+    <button 
+      onClick={() => setLoggedIn(!loggedIn)}>
+        {loggedIn ? 'Logout' : 'Login'}
+      </button>
   </div>
 }
 
