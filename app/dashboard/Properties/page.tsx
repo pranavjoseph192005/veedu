1cc5befd60508ca715f5e8c5dc8e8cb25d6fc7b4
Pranav Joseph <pranavjoseph2005@gmail.com>
2025-06-08 19:42:09 -0500

    fixed issue where it would add multiple houses if user clicked add house multiple times

--- app/dashboard/Properties/page.tsx
+++ app/dashboard/Properties/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import FullPageTable from './components/page' // adjust path as needed
+import Link from 'next/link'
 
 const DashboardPage = () => {
   return (
     <div className="grid grid-flow-col grid-rows-3 gap-4 p-6 bg-gray-50 min-h-screen">
       <div className="row-span-2 col-span-2 bg-white border border-gray-200 rounded-xl shadow-md p-4 flex flex-col">
-      <h2 className="text-lg font-semibold mb-2">Properties</h2>
-          <FullPageTable compact />
+        <div className="flex justify-between items-center mb-3">
+          <h2 className="text-gray-700 font-semibold text-lg">Properties</h2>
+          <Link href="/dashboard/Properties/PropertiesDepth" className="text-sm text-blue-600 hover:underline">
+            View All
+          </Link>
+        </div>
+        <h2 className="text-lg font-semibold mb-2">Properties</h2>
+        <FullPageTable compact />
       </div>
 
 
